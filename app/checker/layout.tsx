79eb358c883025d5136bd929214cb1421ce7a925
hgphuc <hgphuc@hcmiu.edu.vn>
2024-09-11 15:21:13 +0700

    Update checker

---
+++ app/checker/layout.tsx
@@ -0,0 +1,7 @@
+export default function CheckerLayout({
+    children,
+}: {
+    children: React.ReactNode
+}) {
+    return <section>{children}</section>
+}
---
+++ app/checker/page.tsx
@@ -0,0 +1,12 @@
+export default async function Page() {
+    let data = await fetch('https://script.google.com/macros/s/AKfycbwixze4oX1olg_b6xkNOfZdnQKHrR1KnFKVJZn9te_iAh4dN3gtaIon0KBUMLtvmSw3Gw/exec?id=m0opoi59')
+    let posts = await data.json()
+    debugger;
+    return (
+      <ul>
+        {Object.keys(posts).map((post) => (
+          <li>{post}: {posts[post]}</li>
+        ))}
+      </ul>
+    )
+  }
